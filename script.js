f3417b8e260ae298ab67a2fd5267d2f289f47c62
chiseydd <65605748+chiseydd@users.noreply.github.com>
2023-02-28 09:27:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+const authorize = ({
+  default_avatar_id: defaultAvatarId,
+  display_name: displayName,
+}) => {
+  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+  const nameHtml = `<div class="name">${displayName}</div>`;
+
+  document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+};
+
+// Делаем запрос за инфой о пользователе.
+const fetchYandexData = (token) =>
+  fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
+    (res) => res.json()
+  );
+
+window.onload = () => {
+  document.getElementById("suggest").onclick = () => {
+    YaAuthSuggest.init({
+         client_id: '6cc6be492812461fba70ca07defc3f5c',
+         response_type: 'token',
+         redirect_uri: 'https://study-n88v.vercel.app/oauth.html'
+      },
+      'https://study-n88v.vercel.app/'
+   )
+   .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
+  };
+  document.getElementById("button").onclick = () => {
+    // TODO button
+  };
+};
+
 const menuMobile = document.querySelector('.menu__mobile'),
       spansMenu = document.querySelectorAll('.menu__mobile-btn'),
       popupMenu = document.querySelector('.popup__menu'),
